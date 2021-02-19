8ee68d0cbd6a96aeef6557c53f7ea14a1d87f714
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-02-19 17:54:56 -0300

    repo renames

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 # !/usr/bin/env sh
 
-GIT_DOCS="git@github.com:chevereto/docs.git"
-GIT_HOSTING="git@github.com:chevereto/v3-docs.git"
+GIT_DOCS="git@github.com:chevereto/v3-docs.git"
+GIT_HOSTING="git@github.com:chevereto/v3-docs-hosting.git"
 CNAME="v3-docs.chevereto.com"
